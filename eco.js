afb395e913f8dd55aa966e6bcff815d23285c176
Niels Larmuseau <nielslarmuseau@gmail.com>
2018-04-29 16:28:28 +0200

    eco players

--- eco.js
+++ eco.js
@@ -1,36 +1,44 @@
 const request = require("request");
 const ecoconfig = require("./data/ecoconfig.json");
-module.exports = function() { 
-    this.starteco = function starteco (client, discord){
-        setInterval(function () { 
+module.exports = function () {
+    this.starteco = function starteco(client, discord) {
+        setInterval(function () {
             let statuschannel = client.channels.get(ecoconfig.serverstatuschannel);
-            request("http://query.li/api/eco/"+ecoconfig.serverip+'/'+ecoconfig.serverport, (err, res, body) => {
-                if (err) return;
+            request("http://query.li/api/eco/" + ecoconfig.serverip + '/' + ecoconfig.serverport, (err, res, body) => {
+                if (err) statuschannel.send(err); return;
                 try {
                     let contents = JSON.parse(body);
-                    if(contents.status.error){
-                        statuschannel.send(contents.status.message)
+                    if (contents.status.error) { statuschannel.send(contents.status.message) }
+                    else {
+                        request("http://query.li/api/eco/" + ecoconfig.serverip + '/' + ecoconfig.serverport, (err, res, body) => {
+                            if (err) statuschannel.send(err); return;
+                            try {
+                                let ecoapi = JSON.parse(body);
+                                let embed = new discord.RichEmbed();
+                                embed.setAuthor("Server status").setDescription("The Legion eco server status");
+                                embed.addField("Server", contents.game.info.server_name, false);
+                                embed.addField("serverip", ecoconfig.serverip + ':' + ecoconfig.serverport, false);
+                                embed.addField("Game version", contents.game.info.version, false);
+                                embed.addField("Players", contents.game.info.max_players, false);
+                                embed.addField("Players online", ecoapi.is_online, false);
+                                embed.addField("Map size", contents.game.info.map, false);
+                                embed.addField("Economy", contents.game.info.details.EconomyDesc, false);
+                                embed.addField("World Leader", contents.game.info.details.Leader, false);
+                                embed.setTimestamp();
+                                statuschannel.fetchMessages({ limit: 1 }).then(messages => messages.array()[0].edit(embed));
+                            }
+                            catch (err) {
+                                console.log(err);
+                                statuschannel.sendMessage("ERR :disappointed:" + err);
+                            };
+                        });
                     }
-                    else{
-                        let embed = new discord.RichEmbed();
-                        embed.setAuthor("Server status").setDescription("The Legion eco server status");
-                        embed.addField("Server", contents.game.info.server_name ,false);
-                        embed.addField("serverip", ecoconfig.serverip+':'+ecoconfig.serverport ,false);
-                        embed.addField("Game version", contents.game.info.version ,false);
-                        embed.addField("Players", contents.game.info.max_players ,false);
-                        embed.addField("Players online", contents.game.info.player_count ,false);
-                        embed.addField("Map size", contents.game.info.map ,false);
-                        embed.addField("Economy", contents.game.info.details.EconomyDesc ,false);
-                        embed.addField("World Leader", contents.game.info.details.Leader ,false);
-                        embed.setTimestamp();
-                        statuschannel.fetchMessages({limit: 1}).then(messages => messages.array()[0].edit(embed));
-                    }
-                } 
-                catch(err){
-                        console.log(err);
-                        //statuschannel.sendMessage("ERR :disappointed:");
+                }
+                catch (err) {
+                    console.log(err);
+                    statuschannel.sendMessage("ERR :disappointed:" + err);
                 };
-            });  
-        }, 30000); 
+            });
+        }, 60000);
     }
 }
