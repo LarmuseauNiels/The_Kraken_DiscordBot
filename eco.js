66327fa51699626afd9c7cba09743e09a79620e3
Niels Larmuseau <nielslarmuseau@gmail.com>
2018-04-29 16:51:23 +0200

    update eco command

--- eco.js
+++ eco.js
@@ -3,42 +3,50 @@ const ecoconfig = require("./data/ecoconfig.json");
 module.exports = function () {
     this.starteco = function starteco(client, discord) {
         setInterval(function () {
+
             let statuschannel = client.channels.get(ecoconfig.serverstatuschannel);
             request("http://query.li/api/eco/" + ecoconfig.serverip + '/' + ecoconfig.serverport, (err, res, body) => {
-                if (err) statuschannel.send(err); return;
+
+                if (err) console.log("ERR eco 10"+err);
                 try {
                     let contents = JSON.parse(body);
-                    if (contents.status.error) { statuschannel.send(contents.status.message) }
+                    if (contents.status.error) { console.log("ERR eco 13"+err); }
                     else {
-                        request("http://query.li/api/eco/" + ecoconfig.serverip + '/' + ecoconfig.serverport, (err, res, body) => {
-                            if (err) statuschannel.send(err); return;
-                            try {
-                                let ecoapi = JSON.parse(body);
-                                let embed = new discord.RichEmbed();
-                                embed.setAuthor("Server status").setDescription("The Legion eco server status");
-                                embed.addField("Server", contents.game.info.server_name, false);
-                                embed.addField("serverip", ecoconfig.serverip + ':' + ecoconfig.serverport, false);
-                                embed.addField("Game version", contents.game.info.version, false);
-                                embed.addField("Players", contents.game.info.max_players, false);
-                                embed.addField("Players online", ecoapi.is_online, false);
-                                embed.addField("Map size", contents.game.info.map, false);
-                                embed.addField("Economy", contents.game.info.details.EconomyDesc, false);
-                                embed.addField("World Leader", contents.game.info.details.Leader, false);
-                                embed.setTimestamp();
-                                statuschannel.fetchMessages({ limit: 1 }).then(messages => messages.array()[0].edit(embed));
-                            }
-                            catch (err) {
-                                console.log(err);
-                                statuschannel.sendMessage("ERR :disappointed:" + err);
-                            };
-                        });
+                        sendmsg(client,discord,ecoconfig,contents,statuschannel);
                     }
                 }
                 catch (err) {
-                    console.log(err);
+                    console.log("ERR eco 19"+err);
                     statuschannel.sendMessage("ERR :disappointed:" + err);
                 };
             });
-        }, 30000);
+        }, 5000);
     }
+}
+
+var sendmsg = function (client,discord,ecoconfig,contents,statuschannel) {
+    console.log("executing 3 ");
+    request("https://ecoservers.io/api/?object=servers&element=detail&key=" + ecoconfig.ecoserversapikey, (err, res, body) => {
+        console.log("executing 4");
+        if (err) console.log("ERR eco 31"); 
+        try {
+            let ecoapi = JSON.parse(body);
+            let embed = new discord.RichEmbed();
+            embed.setAuthor("Server status").setDescription("The Legion eco server status");
+            embed.addField("Server", contents.game.info.server_name, false);
+            embed.addField("serverip", ecoconfig.serverip + ':' + ecoconfig.serverport, false);
+            embed.addField("Game version", contents.game.info.version, false);
+            embed.addField("Players", contents.game.info.max_players, false);
+            embed.addField("Players online", ecoapi.is_online, false);
+            embed.addField("Map size", contents.game.info.map, false);
+            embed.addField("Economy", contents.game.info.details.EconomyDesc, false);
+            embed.addField("World Leader", contents.game.info.details.Leader, false);
+            embed.setTimestamp();
+            statuschannel.fetchMessages({ limit: 1 }).then(messages => messages.array()[0].edit(embed));
+        }
+        catch (err) {
+            console.log("ERR eco 48");
+            statuschannel.sendMessage("ERR :disappointed:" + err);
+        };
+    });
 }
