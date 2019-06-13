8dbe27234a34df75885a7a46784094d9228e7f8f
Niels Larmuseau <nielslarmuseau@gmail.com>
2019-06-13 11:24:06 +0200

    upgrading channel subs

--- modules/chansubs.js
+++ modules/chansubs.js
@@ -4,16 +4,16 @@ module.exports = function (client) {
     console.log("loading subscription module");
     client.on('messageReactionAdd', (reaction, user) => {
         if(reaction.message.channel.id  == subconfig.channelid){
-            try{reaction.message.guild.member(user).addRole(reaction.message.guild.roles.find("name", reaction.message.content));}
+            try{reaction.message.guild.member(user).addRole(reaction.message.guild.roles.find(val => val.name == reaction.message.content));}
             catch(e){console.log(e);}
         }
     });
     client.on('messageReactionRemove', (reaction, user) => {
         if(reaction.message.channel.id  == subconfig.channelid){
-            try{reaction.message.guild.member(user).removeRole(reaction.message.guild.roles.find("name", reaction.message.content));}
+            try{reaction.message.guild.member(user).removeRole(reaction.message.guild.roles.find(val => val.name == reaction.message.content));}
             catch(e){console.log(e);}
         }
     });
-    client.channels.get(subconfig.channelid).fetchMessages({limit: 50});
+    client.channels.get(subconfig.channelid).fetchMessages({limit: 200});
 }
 
