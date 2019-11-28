722ccbb41ade2f3f57eedd90774155f392353a97
Niels <nielslarmuseau@gmail.com>
2019-11-28 16:05:48 +0100

    fix

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
 const Discord = require("discord.js");
 const fs = require("fs");
 const ytdl = require('ytdl-core');
+class Client extends Discord.Client {
+    constructor() {
+      super();
+      this.commands = new Discord.Collection();
+      this.prefix = "!";
+      this.discord = Discord;
+      this.queue = new Map();
+
+    }
+
+    playsong(message, song) {
+		const queue = message.client.queue;
+		const guild = message.guild;
+        const serverQueue = queue.get(message.guild.id);
+        
+		if (!song) {
+			serverQueue.voiceChannel.leave();
+			queue.delete(guild.id);
+			return;
+		}
+	
+		const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
+			.on('end', () => {
+				console.log('Music ended!');
+				serverQueue.songs.shift();
+				this.playsong(message, serverQueue.songs[0]);
+			})
+			.on('error', error => {
+				console.error(error);
+			});
+		dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
+	}
+}
+
 const client = new Client();
 
 client.on("ready", () => {
@@ -56,38 +90,3 @@ function loadmodules(){
     require("./modules/chansubs.js")(client);
 }
 
-/* */ /* Client class */ /* */
-
-class Client extends Discord.Client {
-    constructor() {
-      super();
-      this.commands = new Discord.Collection();
-      this.prefix = "!";
-      this.discord = Discord;
-      this.queue = new Map();
-
-    }
-
-    playsong(message, song) {
-		const queue = message.client.queue;
-		const guild = message.guild;
-        const serverQueue = queue.get(message.guild.id);
-        
-		if (!song) {
-			serverQueue.voiceChannel.leave();
-			queue.delete(guild.id);
-			return;
-		}
-	
-		const dispatcher = serverQueue.connection.playStream(ytdl(song.url))
-			.on('end', () => {
-				console.log('Music ended!');
-				serverQueue.songs.shift();
-				this.playsong(message, serverQueue.songs[0]);
-			})
-			.on('error', error => {
-				console.error(error);
-			});
-		dispatcher.setVolumeLogarithmic(serverQueue.volume / 5);
-	}
-}
