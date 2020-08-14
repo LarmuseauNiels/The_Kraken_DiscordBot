e878c38988b5e0dfe9c4f467318a0861b89b31da
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-08-14 20:13:16 +0200

    working chan subs

--- modules/chansubs.js
+++ modules/chansubs.js
@@ -8,7 +8,6 @@ module.exports = function (client) {
                 reaction.message.guild.roles.fetch()
                 .then(
                     roles => {
-                        console.log(roles.cache.values);
                         reaction.message.guild.member(user).roles.add(Array.from(roles.cache.values()).find(val => val.name == reaction.message.content));
                     })
                 .catch(err => console.log(err))
@@ -22,7 +21,7 @@ module.exports = function (client) {
                 reaction.message.guild.roles.fetch()
                 .then(
                     roles => {
-                        reaction.message.guild.member(user).removeRole(roles.array.find( val => val.name == reaction.message.content));
+                        reaction.message.guild.member(user).roles.remove(Array.from(roles.cache.values()).find(val => val.name == reaction.message.content));
                     })
                 .catch(err => console.log(err))
             }
