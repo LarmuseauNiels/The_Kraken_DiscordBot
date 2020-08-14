7fbb7dcefab456e131d47c915caf76ab91fb60c2
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-08-14 19:32:46 +0200

    d

--- modules/chansubs.js
+++ modules/chansubs.js
@@ -8,6 +8,7 @@ module.exports = function (client) {
                 reaction.message.guild.roles.fetch()
                 .then(
                     roles => {
+                        console.log(roles);
                         reaction.message.guild.member(user).addRole(roles.array.find(val => val.name == reaction.message.content));
                     })
                 .catch(err => console.log(err))
