681fc78c4f03997a042df37afec138ce2e41d4b1
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-08-15 12:25:31 +0200

    tracking multiple channels

--- modules/statistics.js
+++ modules/statistics.js
@@ -1,6 +1,6 @@
 const mysql = require('mysql');
 const cron = require('node-cron');
-//const config = require("../data/statsconfig.json");
+const config = require("../data/statsconfig.json");
 var knownUserCache = []
 var connection = mysql.createConnection({
   host     : process.env.DBHOST,
@@ -20,24 +20,26 @@ module.exports = function (client) {
 
     cron.schedule('35 0,15,30,45 * * * *', () => {
         console.log('running a cron job');
-        
-        client.channels.fetch('530537522921734178', false)
-        .then(channel => Array.from(channel.members.values()).forEach(member => {
-            if(!knownUserCache.includes(member.id)){
+        config.trackedChannels.forEach(channelID =>{
+            client.channels.fetch(channelID, false)
+            .then(channel => Array.from(channel.members.values()).forEach(member => {
+                if(!knownUserCache.includes(member.id)){
+                    connection.query(
+                        'INSERT INTO Members (ID,DisplayName,avatar) VALUES (?,?,?)',
+                        [member.user.id, member.user.username, member.user.avatar], function (error, results, fields) {
+                            if(error != null){ console.log(error)}
+                        });
+                    knownUserCache.push(member.id)
+                }
                 connection.query(
-                    'INSERT INTO Members (ID,DisplayName,avatar) VALUES (?,?,?)',
-                    [member.user.id, member.user.username, member.user.avatar], function (error, results, fields) {
-                        if(error != null){ console.log(error)}
-                    });
-                knownUserCache.push(member.id)
-            }
-            connection.query(
-            'INSERT INTO VoiceConnected (ID, ChannelID) VALUES (?, ?)',
-            [member.user.id, '530537522921734178'], function (error, results, fields) {
-                if(error != null){ console.log(error)}
-            });
-        }) )
-        .catch( err => console.log(err));
+                'INSERT INTO VoiceConnected (ID, ChannelID) VALUES (?, ?)',
+                [member.user.id, channelID], function (error, results, fields) {
+                    if(error != null){ console.log(error)}
+                });
+            }) )
+            .catch( err => console.log(err));
+        });
+        
     });
 }
 
