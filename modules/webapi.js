28714a78caa3f2dde9290ddb00f06f8e83f0b56f
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-08-15 14:46:46 +0200

    web api test

---
+++ modules/webapi.js
@@ -0,0 +1,20 @@
+const mysql = require('mysql');
+const config = require("../data/webapi.json");
+const express = require('express')
+const app = express()
+
+var connection = mysql.createConnection({
+  host     : process.env.DBHOST,
+  user     : 'root',
+  password : process.env.DBPASS,
+  database : 'discordstats'
+});
+
+
+module.exports = function () {
+    app.get('/', function (req, res) {
+        res.send('API test page.')
+    })
+
+    app.listen(80)
+}
