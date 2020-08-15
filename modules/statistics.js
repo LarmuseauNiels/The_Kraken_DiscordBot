a9251e81499edffe11b5ff50998a8a0ee0bd5a90
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-08-15 13:30:59 +0200

    stats

--- modules/statistics.js
+++ modules/statistics.js
@@ -2,6 +2,7 @@ const mysql = require('mysql');
 const cron = require('node-cron');
 const config = require("../data/statsconfig.json");
 var knownUserCache = []
+var trackedChannels = []
 var connection = mysql.createConnection({
   host     : process.env.DBHOST,
   user     : 'root',
@@ -15,12 +16,16 @@ module.exports = function (client) {
         'Select ID from Members', function (error, results, fields) {
             if(error != null){ console.log(error)}
             results.forEach(result => knownUserCache.push(result.ID))
-            console.log(knownUserCache)
+        });
+    connection.query(
+        'Select ID from Channel', function (error, results, fields) {
+            if(error != null){ console.log(error)}
+            results.forEach(result => trackedChannels.push(result.ID))
         });
 
-    cron.schedule('35 0,15,30,45 * * * *', () => {
+    cron.schedule('30 0,15,30,45 * * * *', () => {
         console.log('running a cron job');
-        config.trackedChannels.forEach(channelID =>{
+        trackedChannels.forEach(channelID =>{
             client.channels.fetch(channelID, false)
             .then(channel => Array.from(channel.members.values()).forEach(member => {
                 if(!knownUserCache.includes(member.id)){
