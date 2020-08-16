dcb80dc0902a01d6f2292b1efd238696a1f1b562
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-08-16 23:10:37 +0200

    new queries

--- modules/webapi.js
+++ modules/webapi.js
@@ -4,9 +4,6 @@ const express = require('express');
 var cors = require('cors');
 const app = express();
 
-
-
-
 module.exports = function (client) {
     app.use(cors());
 
@@ -19,22 +16,47 @@ module.exports = function (client) {
         var responce;
         client.DBconnection.query(
             'select timestamp,count(*) as online from VoiceConnected group by timestamp',
-             function (error, results, fields) {
+            function (error, results, fields) {
                 if(error != null){ 
                     console.log(error)
                     res.send(JSON.stringify("Failure"))
                 }
                 else{
                     res.send(JSON.stringify(results))
-                }
-                //results.forEach(result => knownUserCache.push(result.ID))
-            });
-       
+            }
+        });
+    })
 
-        //
+    app.get('/channelActivity', function (req, res) {
+        var responce;
+        client.DBconnection.query(
+           "select Channel.ChannelName, count(*) from VoiceConnected left join Channel on VoiceConnected.ChannelID = Channel.ID "+
+           " WHERE VoiceConnected.TimeStamp >= curdate() - INTERVAL DAYOFWEEK(curdate())+6 DAY group by Channel.ChannelName",
+            function (error, results, fields) {
+                if(error != null){ 
+                    console.log(error)
+                    res.send(JSON.stringify("Failure"))
+                }
+                else{
+                    res.send(JSON.stringify(results))
+            }
+        });
+    })
+    app.get('/userActivity', function (req, res) {
+        var responce;
+        client.DBconnection.query(
+           "SELECT Members.DisplayName,VoiceConnected.ID , count(*) FROM VoiceConnected LEFT JOIN Members ON VoiceConnected.ID = Members.ID "+
+           " WHERE VoiceConnected.TimeStamp >= curdate() - INTERVAL DAYOFWEEK(curdate())+6 DAY GROUP BY VoiceConnected.ID" ,
+            function (error, results, fields) {
+                if(error != null){ 
+                    console.log(error)
+                    res.send(JSON.stringify("Failure"))
+                }
+                else{
+                    res.send(JSON.stringify(results))
+            }
+        });
     })
     
-
-
     app.listen(3000)
 }
