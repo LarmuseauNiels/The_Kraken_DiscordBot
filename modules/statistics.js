bd6d5140ba2ad35cd683678e0effe1a05d7fe100
Niels <nielslarmuseau@gmail.com>
2020-08-28 10:10:32 +0200

    adds newly created voicechannels

--- modules/statistics.js
+++ modules/statistics.js
@@ -1,41 +1,25 @@
 
 const cron = require('node-cron');
 const config = require("../data/statsconfig.json");
-var knownUserCache = []
-var trackedChannels = []
+var knownUserCache = [];
+
 
 
 
 module.exports = function (client) {
     console.log("loading statistics module");
     -
-    client.on('channelCreate', (channel) => { // needs to be tested
-        console.log("event ran");
+    client.on('channelCreate', (channel) => { 
         if(channel instanceof (client.discord.VoiceChannel)){
-            console.log("is voice channel");
-            console.log(channel.guild.id);
             if(channel.guild.id == "530537522355240961"){
                 console.log("new flaming palm voice channel made");
                 client.DBconnection.query(
                     'INSERT INTO Channel (ID,ChannelName) VALUES (?,?)',[channel.id,channel.name], function (error, results, fields) {
                         if(error != null){ console.log(error);}
-                        else{
-                            client.DBconnection.query(
-                                'Select ID from Channel', function (error, results, fields) {
-                                    if(error != null){ console.log(error)}
-                                    results.forEach(result => trackedChannels.push(result.ID))
-                                });
-                            console.log(trackedChannels);
-                        }
                     });
             }
         }
     });
-    client.DBconnection.query(
-        'Select ID from Channel', function (error, results, fields) {
-            if(error != null){ console.log(error)}
-            results.forEach(result => trackedChannels.push(result.ID))
-        });
     client.DBconnection.query(
         'Select ID from Members', function (error, results, fields) {
             if(error != null){ console.log(error)}
@@ -43,25 +27,31 @@ module.exports = function (client) {
         });
     cron.schedule('30 0,15,30,45 * * * *', () => {
         console.log('running statistics tracking cron job');
-        trackedChannels.forEach(channelID =>{
-            client.channels.fetch(channelID, false)
-            .then(channel => Array.from(channel.members.values()).forEach(member => {
-                if(!knownUserCache.includes(member.id)){
-                    client.DBconnection.query(
-                        'INSERT INTO Members (ID,DisplayName,avatar) VALUES (?,?,?)',
-                        [member.user.id, member.user.username, member.user.avatar], function (error, results, fields) {
+        client.DBconnection.query(
+            'Select ID from Channel', function (error, results, fields) {
+                if(error != null){ console.log(error)}
+                var trackedChannels = [];
+                results.forEach(result => trackedChannels.push(result.ID));
+                trackedChannels.forEach(channelID =>{
+                    client.channels.fetch(channelID, false)
+                    .then(channel => Array.from(channel.members.values()).forEach(member => {
+                        if(!knownUserCache.includes(member.id)){
+                            client.DBconnection.query(
+                                'INSERT INTO Members (ID,DisplayName,avatar) VALUES (?,?,?)',
+                                [member.user.id, member.user.username, member.user.avatar], function (error, results, fields) {
+                                    if(error != null){ console.log(error)}
+                                });
+                            knownUserCache.push(member.id)
+                        }
+                        client.DBconnection.query(
+                        'INSERT INTO VoiceConnected (ID, ChannelID) VALUES (?, ?)',
+                        [member.user.id, channelID], function (error, results, fields) {
                             if(error != null){ console.log(error)}
                         });
-                    knownUserCache.push(member.id)
-                }
-                client.DBconnection.query(
-                'INSERT INTO VoiceConnected (ID, ChannelID) VALUES (?, ?)',
-                [member.user.id, channelID], function (error, results, fields) {
-                    if(error != null){ console.log(error)}
-                });
-            }) )
-            .catch( err => console.log(err));
-        });   
+                    }) )
+                    .catch( err => console.log(err));
+                });   
+            });
     });
 }
 
