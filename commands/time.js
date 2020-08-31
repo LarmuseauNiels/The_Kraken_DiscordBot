e218f3194ce4ca0dfeb649ecc4a98f9045df1a64
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-08-31 17:11:49 +0200

    update

--- commands/time.js
+++ commands/time.js
@@ -3,7 +3,7 @@ const timezones = require("../data/timezones.json");
 module.exports = {
     name: "time",
     desc: "Displays time for someone's local timezone",
-    example: "!time 10pm cest",
+    example: "\time 10pm cest",
     alias: ["t","timezone"],
     run: (client, message, args) => {
         if (args.length > 0) { 
@@ -27,11 +27,12 @@ module.exports = {
             midnight.setUTCMinutes(minutes);
             midnight.setUTCSeconds(0);
             time = new Date(midnight.getTime() - (offset) * 1000 * 60 * 60);
-            //message.channel.sendEmbed(new (require("discord.js")).RichEmbed().setTimestamp().setDescription("I think it is currently this time for you"));
-            return message.channel.sendEmbed(new (require("discord.js")).RichEmbed().setTimestamp(time).setDescription(`Today, ${args+meridian} ${timezone} converts to:`));
+            //message.channel.sendEmbed(new (require("discord.js")).MessageEmbed().setTimestamp().setDescription("I think it is currently this time for you"));
+            return message.channel.sendEmbed(new (require("discord.js")).MessageEmbed().setTimestamp(time).setDescription(`Today, ${args+meridian} ${timezone} converts to:`));
             }
             catch(e){
-                message.reply("Thats now how you use !time, try something like: !time 10pm cest");
+                console.log(e);
+                message.reply("Thats now how you use \time, try something like: \time 10pm cest");
             }
         } else {
             // assuming 2019, its not a leap year 
@@ -71,8 +72,8 @@ module.exports = {
                         date.setUTCDate(day);
                         date.setUTCHours(parseInt(hour));
                         date.setUTCMinutes(parseInt(minutes));
-                        message.channel.sendEmbed(new (require("discord.js")).RichEmbed().setTimestamp().setDescription("I think it is currently this time for you"));
-                        message.channel.sendEmbed(new (require("discord.js")).RichEmbed().setTimestamp(date).setDescription(`So ${month}/${day} ${inTime} UTC must be`));
+                        message.channel.sendEmbed(new (require("discord.js")).MessageEmbed().setTimestamp().setDescription("I think it is currently this time for you"));
+                        message.channel.sendEmbed(new (require("discord.js")).MessageEmbed().setTimestamp(date).setDescription(`So ${month}/${day} ${inTime} UTC must be`));
                     }).catch(e => {
                         console.error(e);
                         message.channel.send("Incorrect something.");
