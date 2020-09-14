88a5245f07472ba28ecf971ab9cc3ddc1883f140
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-09-14 21:30:01 +0200

    update

--- commands/play.js
+++ commands/play.js
@@ -12,7 +12,7 @@ module.exports = {
             const queue = message.client.queue;
             const serverQueue = client.queue.get(message.guild.id);
 
-            const voiceChannel = message.member.voiceChannel;
+            const voiceChannel = message.member.voice.channel;
             if (!voiceChannel) return message.channel.send('You need to be in a voice channel to play music!');
             const permissions = voiceChannel.permissionsFor(message.client.user);
             if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
