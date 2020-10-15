6f35d92f76c98a3bb0cdcb3e828387dd7fa14828
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-10-15 23:55:08 +0200

    debugger

--- commands/sendtoreciever.js
+++ commands/sendtoreciever.js
@@ -10,6 +10,7 @@ module.exports = {
              function (error, results, fields) {
                 if(error != null){ console.log(error)}
                 if(results != null){
+                    console.log(results);
                     client.users.fetch(results).then(reciever => {
                         reciever.send("Secret santa: '"+ text+ "'");
                         message.reply("Send '" + text + "' to " + reciever.username);
