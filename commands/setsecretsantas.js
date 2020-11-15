1a1c6696052b87899b4f46d4b211051c28e630d3
Niels Larmuseau <nielslarmuseau@gmail.com>
2020-11-15 12:04:58 +0100

    j

--- commands/setsecretsantas.js
+++ commands/setsecretsantas.js
@@ -36,12 +36,12 @@ function setsecretsanta(client, message, args){
                                 //senders.find
                                 // find match for reciever
                                 console.log("matching " + reciever.ID)
-                                let sender = senders.find(sender => (sender.HasINTER == 1 && reciever.RequiresINTER ==1 )  || (sender.HasEU == 1 && reciever.RequiresEU ==1));
+                                let sender = senders.find(sender => (sender.HasINTER == 1 && reciever.RequiresINTER == 1 )  || (sender.HasEU == 1 && reciever.RequiresEU ==1));
                                 
                                 console.log("matched " + sender.ID)
-                                    matches.push({rec:reciever.ID,send:sender.ID});
-                                    recievers.splice(recievers.indexOf(reciever),1);
-                                    senders.splice(senders.indexOf(sender),1);
+                                    // matches.push({rec:reciever.ID,send:sender.ID});
+                                    // recievers.splice(recievers.indexOf(reciever),1);
+                                    // senders.splice(senders.indexOf(sender),1);
                                 
                             });
                             console.log(matches);
